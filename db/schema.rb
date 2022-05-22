d8812808bf14758170273b44bfa3162b9e266496
Kostya Sigarov <ksigaryov@gmail.com>
2022-05-22 22:53:01 +0300

    Add_products table, add information into add_products table, update DB pizzashop.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_191624) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_174625) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "discription"
