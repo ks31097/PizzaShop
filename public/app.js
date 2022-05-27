936cfdd2f47b328106ae666e01ab65a55199a105
Kostya Sigarov <ksigaryov@gmail.com>
2022-05-27 11:03:50 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -20,6 +20,7 @@ function add_to_cart(id)
   // вывод количества item'ов в корзине
   alert('Items in your cart: ' + cart_get_number_of_items());
 }
+
 function cart_get_number_of_items()
 {
   var cnt = 0;
@@ -33,4 +34,19 @@ function cart_get_number_of_items()
         }
   }
   return cnt;
+}
+
+function cart_get_orders()
+{
+  var orders = '';
+  for(var i = 0; i < window.localStorage.length; i++)
+  {
+      var key = window.localStorage.key(i); // получаем ключ
+      var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+        if(key.indexOf('product_') == 0)
+        {
+          orders = orders + key + '=' + value + ',';
+        }
+  }
+  return orders;
 }
